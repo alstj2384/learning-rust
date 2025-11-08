4737b79db62b9db567a50b93e1e545ebbdbf4af9
alstj2384 <ass91160@naver.com>
2025-11-08 14:11:41 +0900

    [5-2] 연습 프로그램: 매개변수를 구조체로 리팩토링

--- 05_Using_structs/_5_2_Example_program/src/main.rs
+++ 05_Using_structs/_5_2_Example_program/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    println!("The area of the rectangle is {} square pixels", area(rect1));
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area(&rect1)
+    );
 }
 
-// 하나의 튜플로 받아 매개변수 의미가 명확해짐.
-// 단, 여전히 인덱스로 표현되어 각 값이 무엇인지 뚜렷히 알기 어려움(높이? 너비?)
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+// 구조체로 받음으로써 각 필드명을 명확히 알 수 있게 되었다.
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
